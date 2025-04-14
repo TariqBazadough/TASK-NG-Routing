f1b55444aebb4acf51b11221824f0c70f3795ec4
Dana Aldulijan <daldulijan@hotmail.com>
2025-04-14 11:59:18 +0300

    Solved

--- src/app/components/search-filter/search-filter.component.ts
+++ src/app/components/search-filter/search-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { DifficultyLevel } from '../../../data/trips';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-filter',
@@ -12,8 +13,17 @@ export class SearchFilterComponent {
   @Output() searchChange = new EventEmitter<string>();
   @Output() filterChange = new EventEmitter<DifficultyLevel>();
 
-  difficultyLevels: DifficultyLevel[] = ['All','Easy', 'Moderate', 'Hard'];
+  difficultyLevels: DifficultyLevel[] = ['All', 'Easy', 'Moderate', 'Hard'];
   activeFilter: DifficultyLevel = 'All';
+
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params) => {
+      const difficulty = params['difficulty'] as DifficultyLevel;
+      if (difficulty && ['Easy', 'Moderate', 'Hard'].includes(difficulty)) {
+        this.activeFilter = difficulty;
+      }
+    });
+  }
 
   onSearch(event: Event) {
     const inputElement = event.target as HTMLInputElement;
--- src/app/components/trip-card/trip-card.component.ts
+++ src/app/components/trip-card/trip-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Trip } from '../../../data/trips';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-trip-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './trip-card.component.html',
   styleUrl: './trip-card.component.css',
 })
