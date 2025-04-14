8086046576a51e4832493a1440d5d3eb58614309
A-M98 <aalmohammad@pifss.gov.kw>
2025-04-14 11:59:38 +0300

    Ahmed Mohammed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'trips',
+    loadComponent: () =>
+      import('./pages/trips-list/trips-list.component').then(
+        (m) => m.TripsListComponent
+      ),
+  },
+  {
+    path: 'trips/:slug',
+    loadComponent: () =>
+      import('./pages/trip/trip.component').then((m) => m.TripComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- src/app/components/trip-details/trip-details.component.ts
+++ src/app/components/trip-details/trip-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { Trip } from '../../../data/trips';
 import { DividerComponent } from '../divider/divider.component';
 
@@ -7,7 +7,7 @@ import { DividerComponent } from '../divider/divider.component';
   standalone: true,
   imports: [DividerComponent],
   templateUrl: './trip-details.component.html',
-  styleUrl: './trip-details.component.css'
+  styleUrl: './trip-details.component.css',
 })
 export class TripDetailsComponent {
   @Input() trip!: Trip;
